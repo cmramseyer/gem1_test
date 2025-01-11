9e170a72f5a0f73d778a0527fd5e1f036643accf
carlos <cmramseyer@gmail.com>
2025-01-10 21:41:29 -0300

    initial commit

---
+++ lib/gem1_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem1_test/version"
+
+module Gem1Test
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gem1_test/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Gem1Test
+  VERSION = "0.1.0"
+end
